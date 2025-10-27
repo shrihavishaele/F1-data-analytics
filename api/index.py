404c5252fdefb91f9e9d682b9110c6cd5f2a8293
Shri Havish Aele <140797823+shrihavishaele@users.noreply.github.com>
2025-10-27 23:17:54 +0530

    edit

--- api/index.py
+++ api/index.py
@@ -2,4 +2,5 @@ from vercel_wsgi import serverless_wsgi
 from app import app
 
 def handler(event, context):
-    return serverless_wsgi.handle_request(app, event, context)
+	# Delegate to WSGI adapter
+	return serverless_wsgi.handle_request(app, event, context)
